36479421bd6bf974e38015800414a00ab9c92513
Brambor <tondadrd@seznam.cz>
2023-05-20 02:29:22 +0200

    squish linebreaks on <ol> tag

--- entries/templatetags/html_corrections.py
+++ entries/templatetags/html_corrections.py
@@ -20,32 +20,35 @@ def squish_ul_linebreaks(value):
 	set A = A.striplines()
 	set B = B.strip()
 	"""
-	ul_index = value.find("<ul>")
+	return squish_linebreaks_procedure(squish_linebreaks_procedure(value, "<ul>", "</ul>"), "<ol>", "</ol>")
+
+def squish_linebreaks_procedure(value, tag, end_tag):
+	ul_index = value.find(tag)
 	replacements_indexes = []  # (list)(<int start, int end>)
 	while ul_index != -1:
 		start = ul_index
-		end_ul_index = value.find("</ul>", start)
+		end_ul_index = value.find(end_tag, start)
 		# counts of ul and \ul
 		ul = 0
 		end_ul = 0
-		next_ul_index = value.find("<ul>", ul_index+1)
+		next_ul_index = value.find(tag, ul_index+1)
 		while ul != end_ul or 0 <= next_ul_index < end_ul_index:
 			if ul < end_ul:
-				x = value.find("<ul>", ul_index+1)
+				x = value.find(tag, ul_index+1)
 				if x == -1:
-					raise ValueError("We were supposed to find <ul>! Invalid html syntax!")
+					raise ValueError(f"We were supposed to find {tag}! Invalid html syntax!")
 				ul_index = x
 				ul += 1
 			else:
-				x = value.find("</ul>", end_ul_index+1)
+				x = value.find(end_tag, end_ul_index+1)
 				if x == -1:
-					raise ValueError("We were supposed to find </ul>! Invalid html syntax!")
+					raise ValueError(f"We were supposed to find {tag}! Invalid html syntax!")
 				end_ul_index = x
 				end_ul += 1
-			next_ul_index = value.find("<ul>", ul_index+1)
+			next_ul_index = value.find(tag, ul_index+1)
 		end = end_ul_index
 		replacements_indexes.append((start,end+5))
-		ul_index = value.find("<ul>", ul_index+1)
+		ul_index = value.find(tag, ul_index+1)
 
 	for start, end in reversed(replacements_indexes):
 		value = value.replace(value[end:], value[end:].strip())
